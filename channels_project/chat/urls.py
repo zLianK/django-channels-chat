46e630162878a282eee95381dd477a2cd9345856
Lían König <lk.liankonig@gmail.com>
2021-10-07 10:05:59 -0300

    Initial Signup and Login front-end

--- channels_project/chat/urls.py
+++ channels_project/chat/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('signup', views.SignUpView.as_view(), name='signup'),
+    path('login', views.LogInView.as_view(), name='login'),
 ]
--- channels_project/chat/views.py
+++ channels_project/chat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class SignUpView(TemplateView):
+    template_name = 'chat/signup.html'
+
+
+class LogInView(TemplateView):
+    template_name = 'chat/login.html'
