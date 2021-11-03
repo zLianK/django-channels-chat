a62f942a4ad26644f41eee68d51acd0e0c89ce16
Lían König <lk.liankonig@gmail.com>
2021-11-03 15:58:29 -0300

    Chat user interface

--- channels_project/channels_project/asgi.py
+++ channels_project/channels_project/asgi.py
@@ -1,10 +1,19 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_project.settings')
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+
+        ])
+    )
 })
